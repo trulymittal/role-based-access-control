fdc838e24c878a4eef03e3b92ffa99204c2ed474
Truly Mittal <trulymittal@gmail.com>
2020-12-17 01:42:06 +0530

    5th video

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const createHttpError = require('http-errors');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const session = require('express-session');
+const connectFlash = require('connect-flash');
 
+// Initialization
 const app = express();
 app.use(morgan('dev'));
 app.set('view engine', 'ejs');
@@ -11,22 +14,46 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Init Session
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      // secure: true,
+      httpOnly: true,
+    },
+  })
+);
+
+app.use(connectFlash());
+app.use((req, res, next) => {
+  res.locals.messages = req.flash();
+  next();
+});
+
+// Routes
 app.use('/', require('./routes/index.route'));
 app.use('/auth', require('./routes/auth.route'));
 app.use('/user', require('./routes/user.route'));
 
+// 404 Handler
 app.use((req, res, next) => {
   next(createHttpError.NotFound());
 });
 
+// Error Handler
 app.use((error, req, res, next) => {
   error.status = error.status || 500;
   res.status(error.status);
   res.render('error_40x', { error });
 });
 
+// Setting the PORT
 const PORT = process.env.PORT || 3000;
 
+// Making a connection to MongoDB
 mongoose
   .connect(process.env.MONGODB_URI, {
     dbName: process.env.DB_NAME,
@@ -37,6 +64,7 @@ mongoose
   })
   .then(() => {
     console.log('💾 connected...');
+    // Listening for connections on the defined PORT
     app.listen(PORT, () => console.log(`🚀 on port ${PORT}`));
   })
   .catch((err) => console.log(err.message));
