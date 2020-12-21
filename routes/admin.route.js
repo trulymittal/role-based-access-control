f904bb0b3aed0fb38fbed0f8f714ae95a20a15a1
Truly Mittal <trulymittal@gmail.com>
2020-12-21 22:34:46 +0530

    added try catch

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -29,45 +29,49 @@ router.get('/user/:id', async (req, res, next) => {
 });
 
 router.post('/update-role', async (req, res, next) => {
-  const { id, role } = req.body;
+  try {
+    const { id, role } = req.body;
 
-  // Checking for id and roles in req.body
-  if (!id || !role) {
-    req.flash('error', 'Invalid request');
-    return res.redirect('back');
-  }
+    // Checking for id and roles in req.body
+    if (!id || !role) {
+      req.flash('error', 'Invalid request');
+      return res.redirect('back');
+    }
 
-  // Check for valid mongoose objectID
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    req.flash('error', 'Invalid id');
-    return res.redirect('back');
-  }
+    // Check for valid mongoose objectID
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      req.flash('error', 'Invalid id');
+      return res.redirect('back');
+    }
 
-  // Check for Valid role
-  const rolesArray = Object.values(roles);
-  if (!rolesArray.includes(role)) {
-    req.flash('error', 'Invalid role');
-    return res.redirect('back');
-  }
+    // Check for Valid role
+    const rolesArray = Object.values(roles);
+    if (!rolesArray.includes(role)) {
+      req.flash('error', 'Invalid role');
+      return res.redirect('back');
+    }
 
-  // Admin cannot remove himself/herself as an admin
-  if (req.user.id === id) {
-    req.flash(
-      'error',
-      'Admins cannot remove themselves from Admin, ask another admin.'
+    // Admin cannot remove himself/herself as an admin
+    if (req.user.id === id) {
+      req.flash(
+        'error',
+        'Admins cannot remove themselves from Admin, ask another admin.'
+      );
+      return res.redirect('back');
+    }
+
+    // Finally update the user
+    const user = await User.findByIdAndUpdate(
+      id,
+      { role },
+      { new: true, runValidators: true }
     );
-    return res.redirect('back');
+
+    req.flash('info', `updated role for ${user.email} to ${user.role}`);
+    res.redirect('back');
+  } catch (error) {
+    next(error);
   }
-
-  // Finally update the user
-  const user = await User.findByIdAndUpdate(
-    id,
-    { role },
-    { new: true, runValidators: true }
-  );
-
-  req.flash('info', `updated role for ${user.email} to ${user.role}`);
-  res.redirect('back');
 });
 
 module.exports = router;
